61c59d7f11cf74b827e3ea4f5faff5646c1c6d1d
Junior Santos <jddsjr98@gmail.com>
2021-03-11 19:42:39 -0300

    create an app with test

---
+++ tests/index.spec.js
@@ -0,0 +1,13 @@
+const index = require('./../src/index')
+describe('Testes automatizados', () => {
+    describe('Função de soma deve retornar true', () => {
+        it('should sum 1 + 1 = 2', () => {
+            const a = 1
+            const b = 1 
+            const result = index.soma(a, b)
+
+            expect(result).toBe(2)
+
+        });
+    });
+});
