8fea3bd8c9faca49fd85991d6ab12321f67ad6fd
Junior Santos <jddsjr98@gmail.com>
2021-03-11 21:44:22 -0300

    add some things to app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+const express = require("express")
+
 const soma = (a, b) => {
     return a + b
 }
 
 module.exports = { soma }
+
+
+
+const app = express()
+
+const route = express.Router()
+
+route.get("/:a/:b", (req, resp) => {
+    const result = soma(Number(req.params.a), Number(req.params.b))
+
+    resp.status(200).send({ result })
+})
+app.use(route)
+
+app.listen(3000 || process.env.PORT, () => {
+    console.log("app rodando")
+})
